f2ec21abacf84ab3e44f142d4e36cac66646bd1d
Imran Matin <imranmatin23@gmail.com>
2020-09-08 20:02:48 -0700

    Added in projects and updated links to open in new pages.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
 
     if (this.props.data) {
       var networks = this.props.data.social.map(function (network) {
-        return <li key={network.name}><a href={network.url}><i className={network.className} target="_blank" rel="noopener noreferrer"></i></a></li>
+        return <li key={network.name}><a href={network.url} target="_blank" rel="noopener noreferrer"><i className={network.className}></i></a></li>
       })
     }
 
@@ -18,8 +18,8 @@ class Footer extends Component {
               {networks}
             </ul>
             <ul className="copyright">
-              <li>&copy; Copyright 2020 <a title="Imran Matin" href="http://imranmatin.com/">Imran Matin</a></li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2020 <a title="Imran Matin" href="http://imranmatin.com/" target="_blank" rel="noopener noreferrer">Imran Matin</a></li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/" target="_blank" rel="noopener noreferrer">Styleshout</a></li>
             </ul>
           </div>
           <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
