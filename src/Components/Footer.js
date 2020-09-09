2d602cbe53f89e6439398142bbb99b18e56ea9ac
Imran Matin <imranmatin23@gmail.com>
2020-09-08 19:29:18 -0700

    Added copyright back in.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,6 +17,10 @@ class Footer extends Component {
             <ul className="social-links">
               {networks}
             </ul>
+            <ul className="copyright">
+              <li>&copy; Copyright 2020 <a title="Imran Matin" href="http://imranmatin.com/">Imran Matin</a></li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+            </ul>
           </div>
           <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
         </div>
